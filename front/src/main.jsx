3419c06c4dc96aa30570ab79cb225773e231a385
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2023-12-24 18:20:43 +0100

    fix design issues

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CalendarDetails from './pages/CalendarDetails.jsx'
+import Summit from './pages/Summit.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "details",
+    element: <CalendarDetails />
+  },
+  {
+    path: "summit",
+    element: <Summit />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
